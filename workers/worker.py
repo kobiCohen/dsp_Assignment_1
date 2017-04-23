60ac246d03a098bd96d192c8ea5c86fdf3782b96
noam-stein <noam.stein12@gmail.com>
2017-04-23 20:33:52 +0300

    try to fix rm

--- workers/worker.py
+++ workers/worker.py
@@ -23,9 +23,12 @@ def clean_pdf_folder():
     clean the pdf download folder and result folder
     :return: None
     """
-    check_call(['sudo', 'rm', '-r', '{0}/'.format(download_pdf_dic)])
-    check_call(['sudo', 'rm', '-r', '{0}/'.format(convert_pdf_dic)])
+    check_call(['sudo', 'rm', '-r', '{0}/*'.format(download_pdf_dic)])
+    check_call(['sudo', 'rm', '-r', '{0}/*'.format(convert_pdf_dic)])
 
+# def create_pdf_folder():
+#     check_call(['mkdir', 'rm', '-r', '{0}/'.format(download_pdf_dic)])
+#     check_call(['mkkdir', 'rm', '-r', '{0}/'.format(convert_pdf_dic)])
 
 def download_pdf(task_pdf, pdf_name):
     """
