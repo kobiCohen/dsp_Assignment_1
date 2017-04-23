91f75dd484e0828c4ab4d83d8fd3f5dcf8ab8ceb
noam-stein <noam.stein12@gmail.com>
2017-04-23 19:26:57 +0300

    support unicode

--- workers/pdf_to_txt.py
+++ workers/pdf_to_txt.py
@@ -6,6 +6,7 @@ def pdf_to_txt(pdf_name):
     pdf_path = '{0}/{1}.pdf'.format(download_pdf_dic, pdf_name)
     txt_path = '{0}/{1}.txt'.format(convert_pdf_dic, pdf_name)
     pdf_txt = None
+
     # rb == open in binary mode
     with open(pdf_path, 'rb') as pdf_file:
         pdf_reader = PyPDF2.PdfFileReader(pdf_file)
@@ -13,5 +14,5 @@ def pdf_to_txt(pdf_name):
         pdf_txt = page_obj.extractText()
 
     with open(txt_path, 'w+') as txt_file:
-        txt_file.write(pdf_txt)
+        txt_file.write(pdf_txt.encode('utf8'))
 
