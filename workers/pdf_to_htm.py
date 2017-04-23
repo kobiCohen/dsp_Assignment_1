59a136eef8ed27ac9341ec34619d2b949067f2d9
noam-stein <noam.stein12@gmail.com>
2017-04-23 21:35:52 +0300

    fix to img

--- workers/pdf_to_htm.py
+++ workers/pdf_to_htm.py
@@ -7,7 +7,7 @@ def pdf_to_html(pdf_name):
     html_path = '{0}/{1}'.format(convert_pdf_dic, pdf_name)
     tar_name = '{0}.tar.gz'.format(convert_pdf_dic, pdf_name)
     tar_path = '{0}/{1}'.format(convert_pdf_dic, tar_name)
-    check_call(['pdftohtml', '-l 1', pdf_path, html_path])
+    check_call('pdftohtml -l 1 {0} {1}'.format(pdf_path, html_path))
     check_call('tar -cvf {0} {1}/*'.format(tar_path, convert_pdf_dic), shell=True)
     return tar_path
 
