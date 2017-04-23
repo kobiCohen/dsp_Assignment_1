315b7a9ae313d5eec36f48e440310aaf7041307d
noam-stein <noam.stein12@gmail.com>
2017-04-23 20:23:07 +0300

    limit to first page

--- workers/pdf_to_img.py
+++ workers/pdf_to_img.py
@@ -5,7 +5,7 @@ from subprocess import check_call
 def pdf_to_png(pdf_name):
     pdf_path = '{0}/{1}.pdf'.format(download_pdf_dic, pdf_name)
     png_path = '{0}/{1}'.format(convert_pdf_dic, pdf_name)
-    check_call(['pdftoppm', '-png', pdf_path, png_path])
+    check_call(['pdftoppm', '-l 1','-png', pdf_path, png_path])
     return png_path
 
 
